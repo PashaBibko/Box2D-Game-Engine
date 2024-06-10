92f930a05a79c0afb8edce1fef3def871f81ba3c
PashaBibko <156938226+PashaBibko@users.noreply.github.com>
2024-06-10 15:50:00 +0100

    Added Box2D library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
 
 int main()
 {
 	sf::RenderWindow window (sf::VideoMode(800, 600), "SFML works!");
+	b2World* world = new b2World(b2Vec2(0.0f, 9.8f));
 
 	while (window.isOpen())
 	{
